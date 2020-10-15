17fe92fe011bc8376c9274d1536802ee8353e5c1
tatsuya828828 <kkjy011894@gmail.com>
2020-10-15 23:32:50 +0900

    Viewから文字を受け取りLogに表示し、文字を変更する処理を追加

--- app/src/main/java/com/kuma/javaandroid/MainActivity.java
+++ app/src/main/java/com/kuma/javaandroid/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Hello, World!");
+        TextView hello = findViewById(R.id.textview_hello);
+        String text = hello.getText().toString();
+        Log.d("MainActivity", text);
+        String text2 = "Hello, Android";
+        hello.setText(text2);
     }
 }
